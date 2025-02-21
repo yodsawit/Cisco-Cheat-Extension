807e43b47e615bfb5f082d39cd8bb90637a3e8a9
Yodsawit <yodsawit44@gmail.com>
2025-02-21 17:43:10 +0700

    fix: change local data name

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const outputElement = document.getElementById("quiz-output");
 
-    chrome.storage.local.get("quizData", function (result) {
-        if (result.quizData && result.quizData.length > 0) {
-            outputElement.textContent = result.quizData;
+    chrome.storage.local.get("correctAnswers", function (result) {
+        if (result.correctAnswers && result.correctAnswers.length > 0) {
+            outputElement.textContent = result.correctAnswers.join("\n");
         } else {
             outputElement.textContent = "No quiz data found.";
         }
